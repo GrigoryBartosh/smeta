6e212ab6f759337c782438dfa6d8a1393679c924
Noobgam <shessmaster12@gmail.com>
2016-08-14 22:31:04 +0300

    Small fix in list layout.

--- app/src/main/java/gbarto/ru/smeta/EditRoomActivity.java
+++ app/src/main/java/gbarto/ru/smeta/EditRoomActivity.java
@@ -1,9 +1,7 @@
 package gbarto.ru.smeta;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -87,23 +85,6 @@ public class EditRoomActivity extends AppCompatActivity {
     @Override
     public void onBackPressed()
     {
-        FragmentManager manager = getSupportFragmentManager();
-        MyDialogFragment myDialogFragment = new MyDialogFragment();
-        myDialogFragment.Message = "Если вы вернётесь, то потеряете всё.";
-        myDialogFragment.Title = "Вы уверены, что хотите вернуться?.";
-        myDialogFragment.PositiveButtonTitle = "Да";
-        myDialogFragment.NegativeButtonTitle = "Нет";
-        myDialogFragment.PositiveClicked = new DialogInterface.OnClickListener()
-        {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
-                Intent temp = new Intent();
-                temp.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                setResult(RESULT_CANCELED, temp);
-                finish();
-            }
-        };
-        myDialogFragment.show(manager, "dialog");
+        finish();
     }
 }
