61c9da765cb4c0b5eb18dab4d727558392677266
GrigoryBartosh <grishabartosh@yandex.ru>
2016-08-17 19:51:58 +0300

    settings interface

--- app/src/main/java/gbarto/ru/smeta/SettingsActivity.java
+++ app/src/main/java/gbarto/ru/smeta/SettingsActivity.java
@@ -1,18 +1,26 @@
 package gbarto.ru.smeta;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class SettingsActivity extends AppCompatActivity {
-    private ImageView mImageView;
+    private ImageView mImageButton;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int SELECT_FILE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +28,8 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
-        mImageView = (ImageView) findViewById(R.id.setings_imageView);
-        mImageView.setOnClickListener(imageView_ocl);
+        mImageButton = (ImageView) findViewById(R.id.settings_imageView);
+        mImageButton.setOnClickListener(imageView_ocl);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -38,24 +46,77 @@ public class SettingsActivity extends AppCompatActivity {
         super.onBackPressed();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        boolean res;
+
+        switch (id)
+        {
+            case R.id.menu_work_help:
+                FragmentManager manager = getSupportFragmentManager();
+                MyDialogFragment myDialogFragment = new MyDialogFragment();
+                myDialogFragment.setTitle(getString(R.string.help));
+                myDialogFragment.setMessage(getString(R.string.settings_about_text));
+                myDialogFragment.setPositiveButtonTitle(getString(R.string.ok));
+                myDialogFragment.setPositiveClicked(new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+                myDialogFragment.setUseNegativeButton(false);
+                myDialogFragment.show(manager, "dialog");
+
+                res = true;
+                break;
+            default:
+                res = super.onOptionsItemSelected(item);
+                break;
+        }
+
+        return  res;
+    }
+
     View.OnClickListener imageView_ocl = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            /*Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
+            }*/
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);//
+            startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
         }
     };
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-            if (resultCode == RESULT_OK) {
+        if (requestCode == SELECT_FILE) {
+            /*if (resultCode == RESULT_OK) {
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 mImageView.setImageBitmap(imageBitmap);
+            }*/
+            Bitmap bm=null;
+            if (data != null) {
+                try {
+                    bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
+            mImageButton.setImageBitmap(bm);
+            mImageButton.setPadding(0, 0, 0, 0);
+            //mImageButton.setBackgroundColor(getColor(R.color.search_choose));
         }
     }
 }
