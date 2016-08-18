bb60e5e8f3b7474b64430d10ce8d5d399ce42b39
GrigoryBartosh <grishabartosh@yandex.ru>
2016-08-18 15:30:42 +0300

    Сохранение фоток

--- app/src/main/java/gbarto/ru/smeta/SettingsActivity.java
+++ app/src/main/java/gbarto/ru/smeta/SettingsActivity.java
@@ -2,13 +2,18 @@ package gbarto.ru.smeta;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
 import android.graphics.Bitmap;
 import android.icu.text.UnicodeSetSpanner;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
